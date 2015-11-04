ed8f5dec9273e9fa248796b9fa5861555f57c93b
Yury <coddev@gmail.com>
2015-11-05 00:36:57 +0300

    angular

--- js/main.js
+++ js/main.js
@@ -1,17 +1,22 @@
-//requirejs.config({
-//    baseUrl: 'lib',
-//    paths: {
-//        app: '../js'
-//    }
-//});
+requirejs.config({
+    //baseUrl: 'lib',
+    paths: {
+       // app: '../js'
+    }
+});
 
 require(['./budgetModel'],
-   function(budgetModel){
+   function(budgetModel, angular){
 
+       var app = angular.module('app', []);
+       app.controller('BudgetController', function($scope) {
+           $scope.test= "John";
+          // $scope.lastName= "Doe";
+       });
 
-        budgetModel.init();
-       budgetModel.addItemProfit("sdfg", 444);
-        budgetModel.print();
+        //budgetModel.init();
+        //budgetModel.addItemProfit("sdfg", 444);
+        //budgetModel.print();
 
 
 
